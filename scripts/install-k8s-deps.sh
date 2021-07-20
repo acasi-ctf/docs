5491f38fc2c7803a140644aa9889323f8c535208
Logan Gorence <logan@gorence.io>
2021-07-19 21:31:09 -0700

    Update script again.

---
+++ scripts/install-k8s-deps.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+set -e
+
+cd /usr/local/bin
+
+echo "Preparing..."
+unlink kubectl 2> /dev/null || true
+
+echo "Installing kubectl..."
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x kubectl
+
+echo "Installing minikube..."
+curl -L "https://github.com/kubernetes/minikube/releases/download/v1.22.0/minikube-linux-amd64" --output minikube
+chmod +x minikube
+
+echo "Installing kustomize..."
+curl -L "https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv4.2.0/kustomize_v4.2.0_linux_amd64.tar.gz" --output kustomize.tar.gz
+tar xzf kustomize.tar.gz
+chmod +x kustomize
+rm kustomize.tar.gz
+
+echo "Installing helm..."
+curl -L "https://get.helm.sh/helm-v3.6.3-linux-amd64.tar.gz" --output helm.tar.gz
+tar xzf helm.tar.gz
+mv linux-amd64/helm .
+chmod +x helm
+rm -r linux-amd64
+rm helm.tar.gz
